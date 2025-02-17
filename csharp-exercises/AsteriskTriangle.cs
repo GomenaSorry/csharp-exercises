90604a6b7ae18403ce0b72f092699432b2867805
Ruzzell Fernandez <114500@nelnet.net>
2025-02-17 16:24:39 +0800

    Add project files.

---
+++ csharp-exercises/AsteriskTriangle.cs
@@ -0,0 +1,26 @@
+﻿namespace csharp_exercises
+{
+    public partial class Program
+    {
+        public class AsteriskTriangle       
+        {
+            public static void Write(int length)
+            {
+                int level = 0;
+                while (level < length)
+                {
+                    level++;
+                    for (int i = 0; i < (length - level); i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int j = 0; j < level; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+}
---
+++ csharp-exercises/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace csharp_exercises
+{
+    public partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the height of the triangle:");
+            string input = Console.ReadLine();
+            int length = int.Parse(input);
+            AsteriskTriangle.Write(length);
+        }
+    }
+}
